f66f88e6fa37af99477b379c2308153990ef191b
dinar <xewelus@gmail.com>
2020-01-15 17:00:43 +0300

    Логирование, актуализация

--- src/Converter/Program.cs
+++ src/Converter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -17,11 +18,11 @@ namespace Converter
 		{
 			Application.ThreadException += (sender, args) => MessageBox.Show(args.Exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-			//Process();
-			TestConvert();
+			//MessageBox.Show("TEST");
+			ProcessFolder();
+			//TestConvert();
 
-
-			//return;
+			return;
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
@@ -31,12 +32,17 @@ namespace Converter
 		private static void TestConvert()
 		{
 			string file = @"D:\git\testenc2\windows-1251.txt";
+			ConvertFile(file);
+		}
+
+		private static void ConvertFile(string file)
+		{
 			Encoding win1251 = Encoding.GetEncoding("windows-1251");
 			string text = File.ReadAllText(file, win1251);
 			File.WriteAllText(file, text, Encoding.UTF8);
 		}
 
-		private static void Process()
+		private static void ProcessFolder()
 		{
 			try
 			{
@@ -53,9 +59,13 @@ namespace Converter
 					return;
 				}
 
+				folder = Path.Combine(folder, @".git-rewrite\t");
+				if (!Directory.Exists(folder)) return;
+
 				DirectoryAnalyzer analyzer = new DirectoryAnalyzer();
 				analyzer.Error += AnalyzerOnError;
-				analyzer.Win1251Finded += AnalyzerOnAsciiFinded;
+				analyzer.Win1251Finded += AnalyzerWin1251Finded;
+				analyzer.ProcessDir(folder);
 			}
 			catch (Exception ex)
 			{
@@ -63,14 +73,22 @@ namespace Converter
 			}
 		}
 
-		private static void AnalyzerOnAsciiFinded(string file, CharsetDetector detector)
+		private static void AnalyzerWin1251Finded(string file, CharsetDetector detector)
 		{
-			throw new NotImplementedException();
+			Log(file);
+			ConvertFile(file);
 		}
 
 		private static void AnalyzerOnError(string file, Exception exception, CharsetDetector detector)
 		{
-			MessageBox.Show(file + ":\r\n\r\n" + exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Log(string.Format("Ошибка {0}:\r\n{1}\r\n", file, exception));
+			MessageBox.Show(file + ":\r\n\r\n" + exception, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private static void Log(string message)
+		{
+			string logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+			File.AppendAllText(logFile, string.Format("\r\n[{0:dd.MM.yyyy HH.mm:ss}] {1}", DateTime.Now, message), Encoding.UTF8);
 		}
 	}
 }
