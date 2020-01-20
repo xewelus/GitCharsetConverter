1cdad4d0d245a47716966e03018088a16fd1d1a0
dinar <dinar@esterdev.com>
2020-01-20 17:21:30 +0300

    Tests.cs

--- src/Converter/Program.cs
+++ src/Converter/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 using Ude;
 
@@ -14,81 +17,37 @@ namespace Converter
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
-			Application.ThreadException += (sender, args) => MessageBox.Show(args.Exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Application.ThreadException += OnApplicationOnThreadException;
 
 			//MessageBox.Show("TEST");
-			ProcessFolder();
+
+			//if (args.Length > 0)
+			//{
+			//	MessageBox.Show(args[0]);
+			//}
+
+			//return;
+
+			//ProcessFolder();
 			//TestConvert();
 
-			return;
+			//return;
 
+			ShowMainForm();
+		}
+
+		private static void ShowMainForm()
+		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
 		}
 
-		private static void TestConvert()
+		private static void OnApplicationOnThreadException(object sender, ThreadExceptionEventArgs args)
 		{
-			string file = @"D:\git\testenc2\windows-1251.txt";
-			ConvertFile(file);
-		}
-
-		private static void ConvertFile(string file)
-		{
-			Encoding win1251 = Encoding.GetEncoding("windows-1251");
-			string text = File.ReadAllText(file, win1251);
-			File.WriteAllText(file, text, Encoding.UTF8);
-		}
-
-		private static void ProcessFolder()
-		{
-			try
-			{
-				string folder = ConfigurationManager.AppSettings["RepositoryFolder"];
-				if (string.IsNullOrEmpty(folder))
-				{
-					MessageBox.Show("Не задан параметр RepositoryFolder.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-					return;
-				}
-
-				if (!Directory.Exists(folder))
-				{
-					MessageBox.Show("Не найдена папка " + folder, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-					return;
-				}
-
-				folder = Path.Combine(folder, @".git-rewrite\t");
-				if (!Directory.Exists(folder)) return;
-
-				DirectoryAnalyzer analyzer = new DirectoryAnalyzer();
-				analyzer.Error += AnalyzerOnError;
-				analyzer.Win1251Finded += AnalyzerWin1251Finded;
-				analyzer.ProcessDir(folder);
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
-
-		private static void AnalyzerWin1251Finded(string file, CharsetDetector detector)
-		{
-			Log(file);
-			ConvertFile(file);
-		}
-
-		private static void AnalyzerOnError(string file, Exception exception, CharsetDetector detector)
-		{
-			Log(string.Format("Ошибка {0}:\r\n{1}\r\n", file, exception));
-			MessageBox.Show(file + ":\r\n\r\n" + exception, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-		}
-
-		private static void Log(string message)
-		{
-			string logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
-			File.AppendAllText(logFile, string.Format("\r\n[{0:dd.MM.yyyy HH.mm:ss}] {1}", DateTime.Now, message), Encoding.UTF8);
+			MessageBox.Show(args.Exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
 		}
 	}
 }
